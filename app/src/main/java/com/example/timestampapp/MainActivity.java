d06420ac97454bcb2a3940978f921450fba6b2b3
Akn <sibata486@gmail.com>
2021-01-04 16:50:00 +0900

    機能追加 / ボタンを押した時刻がリストビューに挿入されるよう実装

--- app/src/main/java/com/example/timestampapp/MainActivity.java
+++ app/src/main/java/com/example/timestampapp/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.timestampapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    ListView mainListView;
-    TextView recentTimeStampTextView;
+    private ListView mainListView;
+    private ArrayAdapter mainListAdapter;
+    private TextView recentTimeStampTextView;
+    private List<String> timeStampStrings = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +29,23 @@ public class MainActivity extends AppCompatActivity {
         recentTimeStampTextView = findViewById(R.id.recentTimeStampTextView);
         mainListView = findViewById(R.id.mainListView);
 
-        String[] texts = {"test"};
+        timeStampStrings.add("timeStamp1");
 
-        ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,texts);
+        final ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, timeStampStrings);
 
         mainListView.setAdapter(arrayAdapter);
+        mainListAdapter = arrayAdapter;
+
+        // ボタンにイベントリスナーをセットする
+
+        findViewById(R.id.timeStampButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                timeStampStrings.add(new TimeStamp().getDateTimeString());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
