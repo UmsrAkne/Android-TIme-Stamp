7d3d6e9a0bb69fad95ab466f0c1de3a63461fee8
Akn <sibata486@gmail.com>
2021-01-18 16:39:24 +0900

    リファクタリング / ローカルクラスを、それぞれのメソッド内での匿名クラスに変更

--- app/src/main/java/com/example/timestampapp/dbs/DatabaseHelper.java
+++ app/src/main/java/com/example/timestampapp/dbs/DatabaseHelper.java
@@ -18,7 +18,7 @@ public class DatabaseHelper {
         this.userDao = userDao;
     }
 
-    public void insertTimeStamp(TimeStamp ts){
+    public void insertTimeStamp(final TimeStamp timeStamp){
         Executors.newSingleThreadExecutor().submit(
             new Runnable() {
 
@@ -28,7 +28,7 @@ public class DatabaseHelper {
                 public void run() {
                     TimeStamp ts = new TimeStamp();
                     ts.id = userDao.getAll().size() + 1;
-                    ts.msTime = ts.getDateTime().getTime();
+                    ts.msTime = timeStamp.getDateTime().getTime();
                     userDao.insertAll(ts);
                 }
             }
@@ -36,57 +36,39 @@ public class DatabaseHelper {
     }
 
     public void getTimeStamps(ArrayAdapter<String> adapter){
-        BackgroundRead backgroundRead = new BackgroundRead(userDao, adapter);
-        Executors.newSingleThreadExecutor().submit(backgroundRead);
+        final ArrayAdapter adapter_ = adapter;
+        Executors.newSingleThreadExecutor().submit(
+            new Runnable(){
+
+                private final Handler handler = new Handler(Looper.getMainLooper()){ };
+
+                @Override
+                public void run() {
+                    List<TimeStamp> tss = userDao.getAll();
+                    adapter_.clear();
+                    for (TimeStamp ts:tss ) {
+                        adapter_.add( new TimeStamp(new Date(ts.msTime)).getDateTimeString());
+                    }
+
+                    adapter_.notifyDataSetChanged();
+                }
+            }
+        );
     }
 
     public void rewriteRecentTimeStamp(TimeStamp ts){
-        ReadRecentTimeStampTask r = new ReadRecentTimeStampTask(userDao, ts);
-        Executors.newSingleThreadExecutor().submit(r);
-    }
+        final TimeStamp tStamp = ts;
+        Executors.newSingleThreadExecutor().submit(
+            new Runnable(){
+                private final Handler handler = new Handler(Looper.getMainLooper()){ };
 
-    private static class BackgroundRead implements Runnable{
-        private final ArrayAdapter adapter;
-        private UserDao userDao;
-
-        BackgroundRead(UserDao userDao, ArrayAdapter adapter){
-            this.userDao = userDao;
-            this.adapter = adapter;
-        }
-
-        private final Handler handler = new Handler(Looper.getMainLooper()){
-        };
-
-        @Override
-        public void run() {
-            List<TimeStamp> tss = userDao.getAll();
-            adapter.clear();
-            for (TimeStamp ts:tss ) {
-                adapter.add( new TimeStamp(new Date(ts.msTime)).getDateTimeString());
+                @Override
+                public void run() {
+                    Date dt = new Date(userDao.getAll().get(0).msTime);
+                    tStamp.setDate(dt);
+                }
             }
-
-            adapter.notifyDataSetChanged();
-        }
-    }
-
-    private static class ReadRecentTimeStampTask implements Runnable{
-        private final TimeStamp timeStamp;
-        private UserDao userDao;
-
-        ReadRecentTimeStampTask(UserDao userDao, TimeStamp ts){
-            this.userDao = userDao;
-            this.timeStamp = ts;
-        }
-
-        private final Handler handler = new Handler(Looper.getMainLooper()){
-        };
-
-        @Override
-        public void run() {
-            Date dt = new Date(userDao.getAll().get(0).msTime);
-            timeStamp.setDate(dt);
-        }
-
+        );
     }
 
 }
